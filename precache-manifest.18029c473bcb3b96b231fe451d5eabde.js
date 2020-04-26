94b824e76396c5f434318e1f940332ade8c38d99
Adam Lam <adamwlam26@gmail.com>
2020-04-26 14:12:41 -0400

    Updates

---
+++ precache-manifest.18029c473bcb3b96b231fe451d5eabde.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27781f69f53ca5d3465c28c513f8c20a",
+    "url": "/Adam2611/Lynz-Frontend/index.html"
+  },
+  {
+    "revision": "d5dace1f269b40d048d0",
+    "url": "/Adam2611/Lynz-Frontend/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "4969fcdf91abac32711a",
+    "url": "/Adam2611/Lynz-Frontend/static/css/main.caacea82.chunk.css"
+  },
+  {
+    "revision": "d5dace1f269b40d048d0",
+    "url": "/Adam2611/Lynz-Frontend/static/js/2.95283aa5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Adam2611/Lynz-Frontend/static/js/2.95283aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4969fcdf91abac32711a",
+    "url": "/Adam2611/Lynz-Frontend/static/js/main.aebf69d4.chunk.js"
+  },
+  {
+    "revision": "c343ad2c2b6841ee704f",
+    "url": "/Adam2611/Lynz-Frontend/static/js/runtime-main.79b811ef.js"
+  },
+  {
+    "revision": "669c6653fc2750e3d91ed83b72fdfb67",
+    "url": "/Adam2611/Lynz-Frontend/static/media/busy.669c6653.jpg"
+  },
+  {
+    "revision": "7484a4a96bdb3ec6eaba89d012592517",
+    "url": "/Adam2611/Lynz-Frontend/static/media/corona.7484a4a9.jpg"
+  },
+  {
+    "revision": "d3010a4af61da21e10f1fc24efa7400b",
+    "url": "/Adam2611/Lynz-Frontend/static/media/helpingedit.d3010a4a.jpg"
+  },
+  {
+    "revision": "26d4d0be9cbd3921f33d184103d479e7",
+    "url": "/Adam2611/Lynz-Frontend/static/media/store.26d4d0be.jpg"
+  }
+]);
