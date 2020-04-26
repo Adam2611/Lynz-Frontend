e52a3ca9a36651dca1d5753443dd480acbec09a0
Adam Lam <adamwlam26@gmail.com>
2020-04-26 17:46:45 -0400

    Updates

---
+++ precache-manifest.3e2df334590733d6701f3d0d389ef92a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c449aaa7b2747217bd07dc45d180b067",
+    "url": "/Lynz-Frontend/index.html"
+  },
+  {
+    "revision": "d5dace1f269b40d048d0",
+    "url": "/Lynz-Frontend/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9ddb552cddef6ab6c20b",
+    "url": "/Lynz-Frontend/static/css/main.caacea82.chunk.css"
+  },
+  {
+    "revision": "d5dace1f269b40d048d0",
+    "url": "/Lynz-Frontend/static/js/2.95283aa5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Lynz-Frontend/static/js/2.95283aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddb552cddef6ab6c20b",
+    "url": "/Lynz-Frontend/static/js/main.de18ed4f.chunk.js"
+  },
+  {
+    "revision": "73bc45d20b5a64afb0a4",
+    "url": "/Lynz-Frontend/static/js/runtime-main.681f7e31.js"
+  },
+  {
+    "revision": "669c6653fc2750e3d91ed83b72fdfb67",
+    "url": "/Lynz-Frontend/static/media/busy.669c6653.jpg"
+  },
+  {
+    "revision": "7484a4a96bdb3ec6eaba89d012592517",
+    "url": "/Lynz-Frontend/static/media/corona.7484a4a9.jpg"
+  },
+  {
+    "revision": "d3010a4af61da21e10f1fc24efa7400b",
+    "url": "/Lynz-Frontend/static/media/helpingedit.d3010a4a.jpg"
+  },
+  {
+    "revision": "26d4d0be9cbd3921f33d184103d479e7",
+    "url": "/Lynz-Frontend/static/media/store.26d4d0be.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0684a63528761634b34d52cf1c7a78a4.js"
+  "/Lynz-Frontend/precache-manifest.3e2df334590733d6701f3d0d389ef92a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Lynz-Frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
