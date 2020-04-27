388668578194091f7bee3c37b792dd0815255bcf
nicholas-tao <jacksana1@gmail.com>
2020-04-26 20:32:33 -0400

    back to what it was a few hrs ago

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react";
 /*import "bootstrap/dist/css/bootstrap.min.css"; */
-import { HashRouter as Router, Route, withRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import NavbarComp from "./components/NavbarComp.component";
 import Homepage from "./components/Homepage.component";
 import Viewpage from "./components/Viewpage.component";
 import Submitpage from "./components/Submitpage.component";
 import "./App.css";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className="container">
-          <NavbarComp />
-          <Route
-            path={process.env.PUBLIC_URL}
-            exact
-            component={withRouter(Homepage)}
-          />
-          <Route path={"/busyness"} exact component={withRouter(Viewpage)} />
-          <Route path={"/submit"} exact component={withRouter(Submitpage)} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <NavbarComp />
+        <Route path="/Lynz-Frontend" exact component={Homepage} />
+        <Route path="/Lynz-Frontend/busyness" exact component={Viewpage} />
+        <Route path="/Lynz-Frontend/submit" exact component={Submitpage} />
+      </div>
+    </Router>
+  );
 }
+export default App;
